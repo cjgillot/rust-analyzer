898d9f467b400bbe1d1e0c761908163cc93b30dc
Camille GILLOT <gillot.camille@gmail.com>
2019-11-18 21:06:26 +0100

    Associate AST to empty blocks.

--- crates/ra_hir_def/src/body/lower.rs
+++ crates/ra_hir_def/src/body/lower.rs
@@ -145,9 +145,9 @@ where
         self.body.alloc_pat(pat, src)
     }
 
-    fn empty_block(&mut self) -> ExprId {
+    fn empty_block(&mut self, ptr: AstPtr<ast::Expr>) -> ExprId {
         let block = Expr::Block { statements: Vec::new(), tail: None };
-        self.body.body.exprs.alloc(block)
+        self.alloc_expr(block, ptr)
     }
 
     fn missing_expr(&mut self) -> ExprId {
@@ -185,7 +185,7 @@ where
                                 MatchArm { pats: vec![pat], expr: then_branch, guard: None },
                                 MatchArm {
                                     pats: vec![placeholder_pat],
-                                    expr: else_branch.unwrap_or_else(|| self.empty_block()),
+                                    expr: else_branch.unwrap_or_else(|| self.empty_block(syntax_ptr)),
                                     guard: None,
                                 },
                             ];
